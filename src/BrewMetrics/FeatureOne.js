9ff8d4f2cdfb8fa95ea14d44c877f95eb5dc0d85
Micheal Lane <mikeyd.lane@gmail.com>
2018-02-08 17:22:10 -0500

    github link

--- src/BrewMetrics/FeatureOne.js
+++ src/BrewMetrics/FeatureOne.js
@@ -11,7 +11,7 @@ class FeatureOne extends Component {
           <h1>BrewMetrics for iOS</h1>
           <p>BrewMetrics measures the logistics of your beer drinking habits and the habits of your friends.</p>
           <p>Update you beverages in real time and keep track of money spent, drinks had, and your current drunken status.</p>
-          <h3>Built in React Native | <a target="_gitHub" href="https://github.com/Mikeydlane/BrewMetrics">GitHub</a></h3>
+          <h3>Built in React Native | <a target="_gitHub" href="https://github.com/BrewMetrics">GitHub</a></h3>
         <br></br>
         <br></br>
           <img className="scroll" src='./scroll.svg' />
