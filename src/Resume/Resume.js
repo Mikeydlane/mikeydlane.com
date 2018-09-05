807fee07f53ec3c37442caef1c11f8998c265d9b
michael lane <mikeydlane@michaels-MacBook-Pro.local>
2018-09-05 10:28:31 -0400

    resume update

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -6,8 +6,8 @@ class Resume extends Component {
     return (
       <div className="ContactFeature">
         <div className="resumeft1">
-          <img className="resume1" src='./MichaelLaneResume2018.png' />
-          <a target="_resume" href="./MichaelLaneResume2018.pdf"><p>download .pdf</p></a>
+          <img className="resume1" src='./MichaelLaneResume2018a.png' />
+          <a target="_resume" href="./MichaelLaneResume2018a.pdf"><p>download .pdf</p></a>
         </div>
       </div>
     );
--- src/SideBar/index.js
+++ src/SideBar/index.js
@@ -8,6 +8,7 @@ class SideBar extends Component {
       <div className='header'>
         <h1><Link to="/Contact">MICHAEL LANE</Link></h1>
         <h3>FULL STACK WEB DEVELOPER</h3>
+        <h3 className="UX">USER EXPERIENCE DESIGNER</h3>
         <ul>
           <li className="top-link">MY WORK</li>
             <ul className="min-link">
