de366828f61b250ea3b9feae6a0e33e1e28b82ff
michael lane <mikeydlane@michaels-MBP-4.home>
2019-03-22 13:01:35 -0400

    resume fix

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,8 +5,8 @@ class Resume extends Component {
   render() {
     return (
       <div className="resumeft1">
-        <img className="resume1" src='./Michael_Lane_Resume_19.png' />
-        <a target="_resume" href="./Michael_Lane_Resume_19.pdf"><p>download .pdf</p></a>
+        <img className="resume1" src='./Michael_Lane_Resume_2019.png' />
+        <a target="_resume" href="./Michael_Lane_Resume_19-compressed.pdf"><p>download .pdf</p></a>
       </div>
     );
   }
